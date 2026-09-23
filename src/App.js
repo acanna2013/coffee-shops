5f280ba3251c9500d6a876939a35d6deef1ea01c
Anna Chen <anna.chen@compass.com>
2026-09-22 23:14:49 -0400

    update map styling and add search pop up and music capability

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useRef, useEffect, useState } from 'react'
 import mapboxgl from 'mapbox-gl'
+import useSound from 'use-sound';
+import { Pause, Play } from 'lucide-react';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './App.css'
@@ -13,50 +15,97 @@ const INITIAL_ZOOM = 10.12
 function App() {
   const mapRef = useRef()
   const mapContainerRef = useRef()
+  const [playLofiSong, { pause: pauseLofiSong }] = useSound('/lofi_song.mp3', {
+    onend: () => setIsMusicPlaying(false)
+  })
 
   const [center, setCenter] = useState(INITIAL_CENTER)
   const [zoom, setZoom] = useState(INITIAL_ZOOM)
+  const [searchQuery, setSearchQuery] = useState('')
+  const [hasStartedMusic, setHasStartedMusic] = useState(false)
+  const [isMusicPlaying, setIsMusicPlaying] = useState(false)
 
   useEffect(() => {
     mapRef.current = new mapboxgl.Map({
       accessToken: process.env.REACT_APP_MAPBOX_TOKEN,
       container: mapContainerRef.current,
-      center: center,
-      zoom: zoom
+      style: 'mapbox://styles/banaenaed/cmu30lchk002m01qm36qb21g3',
+      center: [-24, 42],
+      zoom: 1
     });
 
-    mapRef.current.on('move', () => {
-      // get the current center coordinates and zoom level from the map
-      const mapCenter = mapRef.current.getCenter()
-      const mapZoom = mapRef.current.getZoom()
-
-      // update state
-      setCenter([ mapCenter.lng, mapCenter.lat ])
-      setZoom(mapZoom)
-    })
+     // Add geolocate control to the map.
+    mapRef.current.addControl(
+      new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true
+        },
+        trackUserLocation: true,
+        showUserHeading: true,
+      })
+    );
 
     return () => {
-      mapRef.current.remove()
-    }
-  }, [])
+      mapRef.current.remove();
+    };
+  }, []);
 
-  const handleButtonClick = () => {
-  mapRef.current.flyTo({
-    center: INITIAL_CENTER,
-    zoom: INITIAL_ZOOM
-  })
+const handleSearchSubmit = (e) => {
+  e.preventDefault()
 }
 
+const handleMusicIconClick = () => {
+  playLofiSong()
+  setHasStartedMusic(true)
+  setIsMusicPlaying(true)
+}
+
+const handlePauseResumeClick = () => {
+  if (isMusicPlaying) {
+    pauseLofiSong()
+    setIsMusicPlaying(false)
+  } else {
+    playLofiSong()
+    setIsMusicPlaying(true)
+  }
+}
 
 return (
   <>
     <div className="sidebar">
-      Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} | Zoom: {zoom.toFixed(2)}
     </div>
-    <button className='reset-button' onClick={handleButtonClick}>
-      Reset
-    </button>
-    <div id='map-container' ref={mapContainerRef} />
+      <div className="fun-coffee-icon-wrapper">
+        <img
+          src='/coffee_cat_icon.png'
+          className="fun-coffee-icon-button"
+          onClick={handleMusicIconClick}
+          alt="Play lofi music"
+        />
+        {isMusicPlaying && (
+          <>
+            <span className="music-note">♪</span>
+            <span className="music-note">♫</span>
+            <span className="music-note">♪</span>
+          </>
+        )}
+        {hasStartedMusic && (
+          <button className="music-pause-resume-button" onClick={handlePauseResumeClick}>
+            {isMusicPlaying ? <Pause /> : <Play />}
+          </button>
+        )}
+      </div>
+    <div id='map-container' ref={mapContainerRef}>
+    <form className='search-coffee' onSubmit={handleSearchSubmit}>
+      <input 
+      type='text'
+      className='search-input'
+      placeholder='what kind of coffee shop are you looking for?'
+      value={searchQuery}
+      onChange={(e) => setSearchQuery(e.target.value)}
+      />
+    </form>
+
+    </div>
   </>
 
   )
